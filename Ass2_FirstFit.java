4013287afa6a01ae0d9f69b7341429dd0ec317c2
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-08-06 10:09:46 +0530

    Create Ass2_FirstFit.java

---
+++ Ass2_FirstFit.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class FirstFitAllocation {
+    public static void main(String[] args) {
+        final int MAX = 25;
+        int[] frag = new int[MAX];
+        int[] b = new int[MAX];
+        int[] f = new int[MAX];
+        int[] bf = new int[MAX]; // block flags (1 if allocated)
+        int[] ff = new int[MAX]; // file to block mapping
+
+        int nb, nf, temp;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\tMemory Management Scheme - First Fit");
+
+        // Input: number of blocks and files
+        System.out.print("Enter the number of blocks: ");
+        nb = sc.nextInt();
+
+        System.out.print("Enter the number of files: ");
+        nf = sc.nextInt();
+
+        // Input: sizes of the blocks
+        System.out.println("\nEnter the size of the blocks:");
+        for (int i = 1; i <= nb; i++) {
+            System.out.print("Block " + i + ": ");
+            b[i] = sc.nextInt();
+        }
+
+        // Input: sizes of the files
+        System.out.println("Enter the size of the files:");
+        for (int i = 1; i <= nf; i++) {
+            System.out.print("File " + i + ": ");
+            f[i] = sc.nextInt();
+        }
+
+        // First Fit Allocation
+        for (int i = 1; i <= nf; i++) {
+            for (int j = 1; j <= nb; j++) {
+                if (bf[j] != 1) { // if block is not allocated
+                    temp = b[j] - f[i];
+                    if (temp >= 0) {
+                        ff[i] = j;
+                        frag[i] = temp;
+                        bf[j] = 1; // mark block as allocated
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Output
+        System.out.println("\nFile No\tFile Size\tBlock No\tBlock Size\tFragment");
+        for (int i = 1; i <= nf; i++) {
+            if (ff[i] != 0) {
+                System.out.println(i + "\t\t" + f[i] + "\t\t" + ff[i] + "\t\t" + b[ff[i]] + "\t\t" + frag[i]);
+            } else {
+                System.out.println(i + "\t\t" + f[i] + "\t\tNot Allocated");
+            }
+        }
+
+        sc.close();
+    }
+}
